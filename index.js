4f664f1237b7d2cce5e8393a5889be172fbc05e0
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-06-06 08:35:00 +0000

    Completed up till delete

--- index.js
+++ index.js
@@ -8,10 +8,58 @@ const app = express();
 app.set('view engine', 'hbs');
 
 wax.on(hbs.handlebars);
-wax.setLayoutPath('.views/layouts');
+wax.setLayoutPath('./views/layouts');
 
+app.use(express.urlencoded({
+    'extended': false
+}));
+
+// Show all books
 app.get('/', function(req,res){
-    res.send("It's alive!")
+    let books = database.getAll();
+    res.render('books.hbs', {
+        books: books
+    });
+})
+
+// Add books
+app.get('/add-book', function(req,res) {
+    res.render('add-book-form.hbs');
+})
+
+app.post('/add-book', function(req, res) {
+    let title = req.body.title;
+    let isbn = req.body.isbn;
+
+    database.addBook(title, isbn);
+
+    res.redirect('/');
+})
+
+// Update books
+app.get('/update-book', function(req, res) {
+    res.render('update-book-form.hbs');
+})
+
+app.post('/update-book', function(req, res) {
+    let id = req.body.id;
+    let title = req.body.title;
+    let isbn = req.body.isbn;
+
+    database.updateBook(id, title, isbn);
+
+    res.redirect('/');
+})
+
+// Delete books
+app.get('/delete-book', function(req, res) {
+    res.render('delete-book-form.hbs');
+})
+
+app.post('/delete-book', function(req, res) {
+    let id = req.body.id;
+    database.deleteBook(id);
+    res.redirect('/');
 })
 
 app.listen(3000, function(){
